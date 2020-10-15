4ddc67551b248cffb9d52ee1a913a582bd891156
Rémi Robichet <remi.robichet@gmail.com>
2020-10-15 18:02:21 +0200

    :sparkles: Add About page

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <p>
+      Utiliser Nuxt.js pour créer une mini appli permettant d'afficher un tableau de tâches/sous-tâches. <br>
+      Utiliser bootstrap-vue; données des tâches au format JSON. <br>
+      Sur un 2ème onlget afficher un Gantt des tâches à l'aide de frappe.io <br>
+      La modification des données depuis la Gantt actualise le tableau des tâches. <br>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style>
+</style>
