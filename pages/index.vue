3466fd9df553adf44b676203b565c941ce3c2e8f
Rémi Robichet <remi.robichet@gmail.com>
2020-10-15 13:19:00 +0200

    :fire: Remove unused component and nuxt text

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        task-gantt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="title">
+      task-gantt
+    </h1>
   </div>
 </template>
 
